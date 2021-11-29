c04f3fbd6d191147179483b638d2e7d0bde8cd70
Николаева Валерия Леонидовна <v.nikolaeva.2018@stud.nstu.ru>
2021-11-29 23:51:50 +0700

    Created pattern

---
+++ pancakehousemenuiterator.h
@@ -0,0 +1,19 @@
+#ifndef PANCAKEHOUSEMENUITERATOR_H
+#define PANCAKEHOUSEMENUITERATOR_H
+
+#include <list>
+#include "iterator.h"
+#include "menuitem.h"
+
+class PancakeHouseMenuIterator : public Iterator
+{
+private:
+    list<MenuItem*> items;
+    int position;
+public:
+    PancakeHouseMenuIterator(list<MenuItem*> items);
+    void* next();
+    bool hasNext();
+};
+
+#endif
